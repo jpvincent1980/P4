fb382549a9a4a762364ee3d62e4c17748def2f54
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-05-06 15:14:08 +0200

    Files and directories update

---
+++ controllers/controllers.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+# 1. Create a new tournament
+# 2. Add 8 players
+# 3. System generates pairs of players for the first round
+# 4. When the first round is over, results are manually entered in the system
+# 5. Repeat steps 3 and 4 for all remaining rounds until tournament is over
+# Entering players rankings
---
+++ views/views.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+# 1. Afficher la liste de tous les joueurs d'un tournoi
+        # par ordre alphabétique
+        # par classement
+# 2. Afficher la liste de tous les tournois
+# 3. Afficher la liste de tous les tours d'un tournoi
+# 4. Afficher la liste de tous les matchs d'un tournoi
+# 5. Exporter ces listes
